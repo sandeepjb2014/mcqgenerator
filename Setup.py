be872bc3fafca96cd694c770e1d99dedacccc644
Sandeep <sandeepb@rudayapowers.com>
2025-09-15 15:23:37 +0530

    MCQGenerator.ipynb file updated

--- Setup.py
+++ Setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenrator',
+    name='RPPL_MCQ_Generator',
     version='0.0.1',
     author='Sandeep Bhujbal',
     author_email='Sandeepb@Rudayapowers.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
+
